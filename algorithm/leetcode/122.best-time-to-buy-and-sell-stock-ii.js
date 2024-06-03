6e9af15e6f898c6e570a488ae4b8372c7a3fbf9c
hujiqing <hujiqing@bytedance.com>
2024-06-03 22:44:03 +0800

    更新

--- algorithm/leetcode/122.best-time-to-buy-and-sell-stock-ii.js
+++ algorithm/leetcode/122.best-time-to-buy-and-sell-stock-ii.js
@@ -1,36 +1,36 @@
 /**
  * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
  * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
- * 
+ *
  * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
- *  
+ *
  * 示例 1:
  * 输入: [7,1,5,3,6,4]
  * 输出: 7
  * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
  *      随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
- * 
+ *
  * 示例 2:
  * 输入: [1,2,3,4,5]
  * 输出: 4
  * 解释: 在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
  *      注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。
  *      因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
- * 
+ *
  * 示例 3:
  * 输入: [7,6,4,3,1]
  * 输出: 0
  * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
- *  
+ *
  * 提示：
  * 1 <= prices.length <= 3 * 10 ^ 4
  * 0 <= prices[i] <= 10 ^ 4
- * 
- * 
+ *
+ *
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfitII = function (prices) {
+const maxProfitII = function (prices) {
   let left = 0;
   let right = 1;
   let sum = 0;
@@ -40,7 +40,7 @@ var maxProfitII = function (prices) {
       right++;
 
       if (right === prices.length) {
-        return sum += prices[right - 1] - prices[left];
+        return (sum += prices[right - 1] - prices[left]);
       }
     } else {
       sum += prices[right - 1] - prices[left];
@@ -54,14 +54,15 @@ var maxProfitII = function (prices) {
 };
 
 // 优化
-var maxProfitIII = function(prices) {
+const maxProfitIII = function (prices) {
   let res = 0;
-  let pl = 0, pr = 1;
-  while(pr < prices.length) {
-    if(prices[pr] > prices[pr - 1]) {
+  let pl = 0,
+    pr = 1;
+  while (pr < prices.length) {
+    if (prices[pr] > prices[pr - 1]) {
       pr++;
-      if(pr !== prices.length) continue;
-    } 
+      if (pr !== prices.length) continue;
+    }
     res += prices[pr - 1] - prices[pl];
     pl = pr;
     pr = pl + 1;
@@ -76,16 +77,26 @@ var maxProfitIII = function(prices) {
 // dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
 // dp[0][0] = 0
 // dp[0][1] = -price[0]
-var maxProfit = function(prices) {
-  if(prices.length < 2) return 0;
+const maxProfit = function (prices) {
+  if (prices.length < 2) return 0;
   const dp = Array.from({ length: prices.length }, () => []);
   dp[0][0] = 0;
   dp[0][1] = -prices[0];
-  for(let i = 1; i < prices.length; i++) {
+  for (let i = 1; i < prices.length; i++) {
     dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
     dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
   }
   return Math.max(dp[dp.length - 1][0], dp[dp.length - 1][1]);
+};
+
+// 2024.6.3
+// 把所有上升段的值加起来
+const maxProfit4 = function (prices) {
+  let res = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) res += prices[i] - prices[i - 1];
+  }
+  return res;
 };
 
 console.log(maxProfit([7, 1, 5, 3, 6, 4]));
--- algorithm/leetcode/125.vaild-palindrome.js
+++ algorithm/leetcode/125.vaild-palindrome.js
@@ -1,55 +1,55 @@
 /**
  * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
  * 说明：本题中，我们将空字符串定义为有效的回文串。
- * 
+ *
  * 示例 1:
  * 输入: "A man, a plan, a canal: Panama"
  * 输出: true
- * 
+ *
  * 示例 2:
  * 输入: "race a car"
  * 输出: false
- * 
+ *
  * @param {string} s
  * @return {boolean}
  */
 
 // 1. API方式
-var isPalindrome = function(s) {
+const isPalindrome = function (s) {
   const pure = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
 
   return pure === pure.split('').reverse().join('');
 };
 
-const getCode = function(s, i) {
+const getCode = function (s, i) {
   const code = s.charCodeAt(i);
-  if(code >= 48 && code <= 57 || code >= 97 && code <= 122) {
+  if ((code >= 48 && code <= 57) || (code >= 97 && code <= 122)) {
     return code;
-  } else if(code >= 65 && code <= 90) {
+  } else if (code >= 65 && code <= 90) {
     return code + 32;
   }
   return -1;
 };
 
 // 头尾指针
-var isPalindromePointer = function(s) {
+const isPalindromePointer = function (s) {
   let left = 0;
   let right = s.length - 1;
 
-  while(left < right) {
+  while (left < right) {
     const lcode = getCode(s, left);
-    if(lcode < 0) {
+    if (lcode < 0) {
       left++;
       continue;
     }
 
     const rcode = getCode(s, right);
-    if(rcode < 0) {
+    if (rcode < 0) {
       right--;
       continue;
     }
 
-    if(lcode !== rcode) {
+    if (lcode !== rcode) {
       return false;
     } else {
       left++;
@@ -58,7 +58,7 @@ var isPalindromePointer = function(s) {
   }
 
   return true;
-}
+};
 
 // console.log(isPalindrome('A man, a plan, a canal: Panama'));
 // console.log(isPalindrome(''));
