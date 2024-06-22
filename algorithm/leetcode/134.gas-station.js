7c00fe5c761a526e1a37267704f41fc4f943d18a
hujiqing <hujiqing@bytedance.com>
2024-06-22 13:31:26 +0800

    更新

--- algorithm/leetcode/134.gas-station.js
+++ algorithm/leetcode/134.gas-station.js
@@ -3,14 +3,14 @@
  * 你有一辆油箱容量无限的的汽车，从第 i 个加油站开往第 i+1 个加油站需要消耗汽油 cost[i] 升。
  * 你从其中的一个加油站出发，开始时油箱为空。
  * 如果你可以绕环路行驶一周，则返回出发时加油站的编号，否则返回 -1。
- * 
- * 说明: 
+ *
+ * 说明:
  * 如果题目有解，该答案即为唯一答案。
  * 输入数组均为非空数组，且长度相同。
  * 输入数组中的元素均为非负数。
- * 
+ *
  * 示例 1:
- * 输入: 
+ * 输入:
  * gas  = [1,2,3,4,5]
  * cost = [3,4,5,1,2]
  * 输出: 3
@@ -22,12 +22,12 @@
  * 开往 2 号加油站，此时油箱有 6 - 4 + 3 = 5 升汽油
  * 开往 3 号加油站，你需要消耗 5 升汽油，正好足够你返回到 3 号加油站。
  * 因此，3 可为起始索引。
- * 
+ *
  * 示例 2:
- * 输入: 
+ * 输入:
  * gas  = [2,3,4]
  * cost = [3,4,3]
- * 
+ *
  * 输出: -1
  * 解释:
  * 你不能从 0 号或 1 号加油站出发，因为没有足够的汽油可以让你行驶到下一个加油站。
@@ -36,25 +36,25 @@
  * 开往 1 号加油站，此时油箱有 3 - 3 + 3 = 3 升汽油
  * 你无法返回 2 号加油站，因为返程需要消耗 4 升汽油，但是你的油箱只有 3 升汽油。
  * 因此，无论怎样，你都不可能绕环路行驶一周。
- * 
- * 
+ *
+ *
  * @param {number[]} gas
  * @param {number[]} cost
  * @return {number}
  */
 
 // 1. 遍历
-var canCompleteCircuit = function(gas, cost) {
+const canCompleteCircuit = function (gas, cost) {
   // 选择起点站
-  for(let i = 0; i < gas.length; i++) {
+  for (let i = 0; i < gas.length; i++) {
     let flag = true;
-    if(cost[i] > gas[i]) continue;
+    if (cost[i] > gas[i]) continue;
 
     let rest = 0;
-    for(let j = i; j < gas.length + i; j++) {
+    for (let j = i; j < gas.length + i; j++) {
       const index = j % gas.length;
 
-      if(rest + gas[index] >= cost[index]) {
+      if (rest + gas[index] >= cost[index]) {
         rest = rest + gas[index] - cost[index]; // 减去耗油
       } else {
         flag = false;
@@ -62,28 +62,28 @@ var canCompleteCircuit = function(gas, cost) {
       }
     }
 
-    if(flag) return i;
+    if (flag) return i;
   }
 
   return -1;
 };
 
 // 2. 优化，一次遍历
-var canCompleteCircuitII = function(gas, cost) {
+const canCompleteCircuitII = function (gas, cost) {
   // 选择起点站
   let start = 0;
-  while(start < gas.length) {
+  while (start < gas.length) {
     let flag = true;
-    if(cost[start] > gas[start]) {
+    if (cost[start] > gas[start]) {
       start++;
       continue;
     }
 
     let rest = 0;
-    for(let j = start; j < gas.length + start; j++) {
+    for (let j = start; j < gas.length + start; j++) {
       const index = j % gas.length;
 
-      if(rest + gas[index] >= cost[index]) {
+      if (rest + gas[index] >= cost[index]) {
         rest = rest + gas[index] - cost[index]; // 减去耗油
       } else {
         flag = false;
@@ -92,21 +92,22 @@ var canCompleteCircuitII = function(gas, cost) {
       }
     }
 
-    if(flag) return start;
+    if (flag) return start;
   }
 
   return -1;
 };
 
 // 3.
-var canCompleteCircuitIII = function(gas, cost) {
+// 思路：只要一圈下来，剩余油量是大于等于0，则一定存在某个位置能够跑完一圈
+const canCompleteCircuitIII = function (gas, cost) {
   let sum = 0;
   let temp = 0;
   let index = 0;
-  for(let i = 0; i < gas.length; i++) {
+  for (let i = 0; i < gas.length; i++) {
     temp += gas[i] - cost[i];
     sum += gas[i] - cost[i];
-    if(temp < 0) {
+    if (temp < 0) {
       temp = 0;
       index = i + 1;
     }
@@ -115,5 +116,5 @@ var canCompleteCircuitIII = function(gas, cost) {
 };
 
 const { logAssert } = require('./tools/LogTools.js');
-logAssert(canCompleteCircuitIII, [1,2,3,4,5], [3,4,5,1,2], 3);
-logAssert(canCompleteCircuitIII, [2,3,4], [3,4,3], -1);
+logAssert(canCompleteCircuitIII, [1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 3);
+logAssert(canCompleteCircuitIII, [2, 3, 4], [3, 4, 3], -1);
--- algorithm/leetcode/137.single-number-ii.js
+++ algorithm/leetcode/137.single-number-ii.js
@@ -1,32 +1,32 @@
 /**
  * 给你一个整数数组 nums ，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 。
  * 请你找出并返回那个只出现了一次的元素。
- * 
+ *
  * 示例 1：
  * 输入：nums = [2,2,3,2]
  * 输出：3
- * 
+ *
  * 示例 2：
  * 输入：nums = [0,1,0,1,0,1,99]
  * 输出：99
- * 
+ *
  * 提示：
  * 1 <= nums.length <= 3 * 104
  * -231 <= nums[i] <= 231 - 1
  * nums 中，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次
- * 
+ *
  * 进阶：你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
- * 
+ *
  * @param {number[]} nums
  * @return {number}
  */
 
 // 1. hash
-var singleNumber = function(nums) {
+const singleNumber = function (nums) {
   const set = new Set();
   const resSet = new Set();
-  for(const num of nums) {
-    if(!set.has(num)) {
+  for (const num of nums) {
+    if (!set.has(num)) {
       set.add(num);
       resSet.add(num);
     } else {
@@ -37,22 +37,22 @@ var singleNumber = function(nums) {
 };
 
 // 2. 排序
-var singleNumberII = function(nums) {
+const singleNumberII = function (nums) {
   nums.sort((a, b) => a - b);
-  for(let i = 0; i < nums.length; i++) {
-    if(nums[i] !== nums[i - 1] && nums[i] !== nums[i + 1]) return nums[i];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1] && nums[i] !== nums[i + 1]) return nums[i];
   }
 };
 
 // 3. 位运算
-var singleNumberIII = function(nums) {
+const singleNumberIII = function (nums) {
   let res = 0;
-  for(let i = 0; i < 32; i++) {
+  for (let i = 0; i < 32; i++) {
     let total = 0;
-    for(const num of nums) {
+    for (const num of nums) {
       total += (num >> i) & 1;
     }
-    if(total % 3 !== 0) {
+    if (total % 3 !== 0) {
       res |= 1 << i;
     }
   }
@@ -60,27 +60,56 @@ var singleNumberIII = function(nums) {
 };
 
 // 4. 数字电路
-var singleNumberIV = function(nums) {
-  let a = 0, b = 0;
-  for(const num of nums) {
+const singleNumberIV = function (nums) {
+  let a = 0;
+  let b = 0;
+  for (const num of nums) {
     const t = (~a & b & num) | (a & ~b & ~num);
     b = (~a & ~b & num) | (~a & b & ~num); // b = ~a & (b ^ num)
     a = t;
   }
   return b;
-}
+};
 
 // 5. 数字电路优化
 // 先计算 b，再用新的 b 替换原来的 b，列出 a 的表达式
-var singleNumberV = function(nums) {
-  let a = 0, b = 0;
-  for(const num of nums) {
+const singleNumberV = function (nums) {
+  let a = 0;
+  let b = 0;
+  for (const num of nums) {
     b = ~a & (b ^ num);
     a = ~b & (a ^ num);
   }
   return b;
-}
+};
 
-const { logAssert } = require('./tools/LogTools.js');
-logAssert(singleNumberIV, [30000,500,100,30000,100,30000,100], 500);
-logAssert(singleNumberIV, [-2,-2,1,1,4,1,4,4,-4,-2], -4);
+/**
+ * 2024.6.20
+ * 按位计算，每个数同位相加 % 3，得到余数，就是只出现一次的数对应位上的数
+ * 过程中可能值出现的值为 0、1、2，因此取两位 a b 来保存
+ * __
+ * ab = ~(a + b)
+ * _     _
+ * ab + ab = a ^ b
+ **************************************************
+ *       __   _
+ * a' = abx + abx
+ *      _ _   __    _  _   _     _
+ * b' = abx + abx = a(bx + bx) = a(b ^ x)
+ */
+const singleNumber6 = function (nums) {
+  let a = 0;
+  let b = 0;
+  for (const n of nums) {
+    const t = a;
+
+    a = (a & ~(b | n)) | (~a & b & n);
+
+    b = ~t & (b ^ n);
+  }
+  return b;
+};
+
+const { logAssert } = require('../tools/LogTools.js');
+logAssert(singleNumber6, [30000, 500, 100, 30000, 100, 30000, 100], 500);
+logAssert(singleNumber6, [-2, -2, 1, 1, 4, 1, 4, 4, -4, -2], -4);
